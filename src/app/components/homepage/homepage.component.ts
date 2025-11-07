09aa429806b545e3b10e6ec3aa350f49ecb7be6e
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-11-07 19:09:34 +0100

    Home's carousel sistemato(statico)

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,20 +1,20 @@
-import { Component, ElementRef, signal, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, model, signal, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Signal } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @Component({
   selector: 'app-homepage',
-  imports: [RouterModule,MatCardModule ],
+  imports: [RouterModule,MatCardModule,MatDatepickerModule ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
-  // longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  // from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  // originally bred for hunting.`;
- 
+  selected = model<Date | null>(null);
 
 
 }
