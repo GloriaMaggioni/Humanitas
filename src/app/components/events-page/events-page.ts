26dd30ce31b34bbdfacee5e6adb0e13247eeb401
Gloria Maggioni <105869401+GloriaMaggioni@users.noreply.github.com>
2025-11-19 19:35:03 +0100

    Add  the logic of the tab's button selected for the events' list

--- src/app/components/events-page/events-page.ts
+++ src/app/components/events-page/events-page.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
+import { NgClass } from "@angular/common";
 @Component({
   selector: 'app-events-page',
-  imports: [MatButtonModule,MatSidenavModule],
+  imports: [MatButtonModule, MatSidenavModule, ],
   templateUrl: './events-page.html',
   styleUrl: './events-page.css'
 })
 export class EventsPage {
+
+selectedIndex: number = 0
+
+  buttonSelected(index: number) {
+    this.selectedIndex = index;
+  }
+
+  tabs = [
+    { label: 'All' },
+    { label: 'Art' },
+    { label: 'Tech' },
+    { label: 'Business' },
+    { label: 'Social' },
+    { label: 'Book' },
+    { label: 'Economy' }
+  ]
+
 
 }
